928eec7dde48472916ad33380bfc202320c1d394
S.A.P.V.Rupasinghe <126545843+PraveenaRupasingha@users.noreply.github.com>
2023-07-04 08:53:02 +0530

    Update Q3.scala

--- Tutorial 04/Q3.scala
+++ Tutorial 04/Q3.scala
@@ -1,13 +1,13 @@
 object Q3{
 
   def main(args: Array[String]): Unit = {
-    println(formatNames("Benny", toUpper))
-    println(formatNames("Niroshan", (name: String) => name.substring(0, 1) + toUpper(name.substring(1, 2)) + name.substring(2)))
-    println(formatNames("Saman", toLower))
-    println(formatNames("Kumara", (name: String) => name.substring(0, 5) + toUpper(name.substring(5))))
+    println(formatNames("Benny") (toUpper))
+    println(formatNames("Niroshan")((name: String) => name.substring(0, 1) + toUpper(name.substring(1, 2)) + name.substring(2)))
+    println(formatNames("Saman")(toLower))
+    println(formatNames("Kumara")((name: String) => name.substring(0, 5) + toUpper(name.substring(5))))
   }
 
   def toUpper(str : String): String = str.toUpperCase()
   def toLower(str: String): String = str.toLowerCase()
-  def formatNames(name: String, callBack: String => String): String = callBack(name)
+  def formatNames(name: String)(callBack: String => String): String = callBack(name)
 }
