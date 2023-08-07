e4df494ee517f32678fd9baa797a49c5f11f8528
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2023-08-07 23:07:14 +0530

    Answer for question 3

--- Tutorial 07/Q3.scala
+++ Tutorial 07/Q3.scala
@@ -1,1 +1,14 @@
+object Q3 extends  App{
+  def isPrime(num: Int): Boolean = {
+    if (num <= 1) false
+    else if (num == 2) true
+    else !(2 to Math.sqrt(num).toInt).exists(x => num % x == 0)
+  }
 
+  def filterPrime(input: List[Int]): List[Int] = {
+    input.filter(num => isPrime(num))
+  }
+  val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val output = filterPrime(input)
+  println(output.mkString(", "))
+}
