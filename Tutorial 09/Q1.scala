4b864b96297259d2c0f1a673449c324f0a9eff5d
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2023-08-22 05:08:47 +0530

    Answer for the question 1

--- Tutorial 09/Q1.scala
+++ Tutorial 09/Q1.scala
@@ -1,11 +1,17 @@
+import scala.io.StdIn
+
 object Q1 extends App{
-  val r: Rational = new Rational(5, 6);
-  println(r.neg);
+  print("Enter the numerator: ")
+  val x = StdIn.readInt()
+  print("Enter the denominator: ")
+  val y = StdIn.readInt()
+
+  val nr: Rational = new Rational(x, y);
+  println(nr.neg);
 }
 
 class Rational(x: Int, y: Int) {
   def numer = x
-
   def denom = y
 
   def neg = new Rational(-this.numer, this.denom)
