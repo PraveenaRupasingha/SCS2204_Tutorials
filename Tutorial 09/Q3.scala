342bb1fd8aefe50e770eb8d9b406654d25ad0e84
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2023-08-22 05:10:43 +0530

    Answer for the question 3

--- Tutorial 09/Q3.scala
+++ Tutorial 09/Q3.scala
@@ -1,14 +1,16 @@
 object Q3 extends App{
-  val account1: Account = new Account("1", "001", 1000)
-  val account2: Account = new Account("2", "002", 100)
-  account1.transfer(account2, 500)
+  val account1: Account = new Account("1", "001", 5000)
+  val account2: Account = new Account("2", "002", 1000)
+
+  account1.transfer(account2, 1000)
+
   println(s"Account 1 balance : ${account1.balance}")
   println(s"Account 2 balance : ${account2.balance}")
 }
 
 class Account(i: String, n: String, b: Double) {
   var id: String = i
-  var accNumber: String = n
+  var accNo: String = n
   var balance: Double = b
 
   def transfer(that: Account, amount: Double) = {
